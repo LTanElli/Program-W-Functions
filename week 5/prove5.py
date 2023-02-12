1b9ff9561813ccafb45c0549b7cb8583b3063e5e
Tanner Elliott <lynnte@byui.edu>
2023-02-11 17:05:12 -0700

    l

--- week 5/prove5.py
+++ week 5/prove5.py
@@ -10,7 +10,7 @@ def main():
     # I thought the above would work. Didn't, not sure why.
     for quantity in range(2):
         for tense in tenses:
-            print(f"{get_determiner(quantity).capitalize()} {get_noun(quantity)} {get_verb(quantity, tense)}.")
+            print(f"{get_determiner(quantity).capitalize()} {get_noun(quantity)} {get_verb(quantity, tense)} {get_prepositional_phrase(quantity)}.")
 
 
 
@@ -114,6 +114,43 @@ def get_verb(quantity, tense):
         "will walk", "will write"]
         word = random.choice(words)
         return word
+
+def get_preposition():
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
+
+    Return: a randomly chosen preposition.
+    """
+    prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+    preposition = random.choice(prepositions)
+    return preposition
+
+def get_prepositional_phrase(quantity):
+    """Build and return a prepositional phrase composed
+    of three words: a preposition, a determiner, and a
+    noun by calling the get_preposition, get_determiner,
+    and get_noun functions.
+
+    Parameter
+        quantity: an integer that determines if the
+            determiner and noun in the prepositional
+            phrase returned from this function should
+            be single or pluaral.
+    Return: a prepositional phrase.
+    """
+    prepostional_phrase = get_prepostion() + " " + get_determiner(quantity) + " " + get_noun(quantity)
+    return prepositional_phrase
 
 
 
